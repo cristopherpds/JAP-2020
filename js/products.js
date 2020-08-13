3b94c628442010381905edc1f60f36bebab74d04
Cristopher Paiva <josuepaiva7@gmail.com>
2020-08-13 11:19:47 -0300

    fix product list

--- js/products.js
+++ js/products.js
@@ -36,10 +36,9 @@ function showCategoriesList(array) {
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(PRODUCTS_URL).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-            productsArray = resultObj.data;
-            //Muestro las categorías ordenadas
+    getJSONData(PRODUCTS_URL).then(function (resObj) {
+        if (resObj.status === "ok") {
+            productsArray = resObj.data;
             showCategoriesList(productsArray);
         }
     });
